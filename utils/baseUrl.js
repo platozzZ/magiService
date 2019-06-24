563d0b6a1cf91c3e6959853cd508df8a1f19fdaa
王鹏 <wangpeng@magi.link>
2019-06-24 19:24:22 +0800

    新增1

--- utils/baseUrl.js
+++
@@ -1,8 +0,0 @@
-const baseUrl = "https://fuwu.magi.link"
-// const baseUrl = "http://192.168.1.30:8080"
-// const baseUrl = "https://test01fuwu.magi.link"
-// module.exports = {
-//   baseUrl: baseUrl
-// }
-
-module.exports = baseUrl
